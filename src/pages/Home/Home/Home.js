d992ee610c2c342c9e7ad81a0b66cb47018aea94
devmdtanvir <officialtanvir@hotmail.com>
2023-01-15 22:04:05 +0600

    I am ready to push code github becouse complite my codding today

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,10 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../../../Context/AuthProvider'
+import React from 'react'
 import { About } from '../About/About'
 import { Carousel } from '../Carousel/Carousel'
 import { Service } from '../Service/Service'
 
 export const Home = () => {
-    const { user } = useContext(AuthContext);
-    console.log(user.email);
+
     return (
         <div>
             <Carousel />
--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -6,7 +6,7 @@ export const Service = () => {
     const [services, setServices] = useState([]);
     console.log(services);
     useEffect(() => {
-        fetch('/service.json')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
